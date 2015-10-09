20bb1e48251def9fa0f930178c9586e7f50c9045
Oleksiy Syrotkin <oleksiy.syrotkin@elca.ch>
2015-10-09 19:04:09 +0200

    added first XML mapping files

---
+++ Tests.Unit/InMemoryDatabaseForXmlMappings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Tool.hbm2ddl;
+
+using Environment = NHibernate.Cfg.Environment;
+
+namespace Tests.Unit {
+
+    public class InMemoryDatabaseForXmlMappings : IDisposable {
+
+        protected Configuration m_config;
+        protected ISessionFactory m_sessionFactory;
+
+
+        public InMemoryDatabaseForXmlMappings() {
+            m_config = new Configuration()
+                    .SetProperty(Environment.ReleaseConnections, "on_close")
+                    .SetProperty(Environment.Dialect, typeof(SQLiteDialect).AssemblyQualifiedName)
+                    .SetProperty(Environment.ConnectionDriver, typeof(SQLite20Driver).AssemblyQualifiedName)
+                    .SetProperty(Environment.ConnectionString, "data source=:memory:")
+                    .AddFile("Mappings/Xml/Employee.hbm.xml");
+            m_sessionFactory = m_config.BuildSessionFactory();
+            Session = m_sessionFactory.OpenSession();
+
+            new SchemaExport(m_config).Execute(true, true, false, Session.Connection, Console.Out);
+        }
+
+        public ISession Session {
+            get;
+            set;
+        }
+
+        public void Dispose() {
+            // Session.Dispose();
+             m_sessionFactory.Dispose();
+        }
+
+    }
+
+}
